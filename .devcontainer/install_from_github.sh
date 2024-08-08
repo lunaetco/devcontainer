bd13c36fe0377fa2ad24e0b7b2b08a6fc5f52e92
Jacob Lee <jakelee8@icloud.com>
2024-08-08 16:31:23 -0700

    Install diesel CLI

--- .devcontainer/install_from_github.sh
+++ .devcontainer/install_from_github.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+
 # Function to display usage information
 usage() {
     echo "Usage: $0 <owner> <repo> [install_dir]"
@@ -33,90 +35,48 @@ aarch64 | arm64)
     ;;
 esac
 
-# Fetch the latest release information
-LATEST_RELEASE=$(curl -s "https://api.github.com/repos/$OWNER/$REPO/releases/latest")
+# Fetch the release information
+RELEASE=$(curl -s "https://api.github.com/repos/$OWNER/$REPO/releases")
 
-# Function to download and install .deb package
-install_deb() {
-    local DEB_URL="$1"
-    local DEB_FILE="${REPO}_latest_${ARCH}.deb"
+DOWNLOAD_URL=$(printf "%s" "$RELEASE" | jq -r "[
+    .[].assets[] |
+    select(.name |
+        test(\"($ARCH_VARIANTS)\"; \"i\") and
+        test(\"linux\"; \"i\")) |
+    { url: .browser_download_url } +
+    (.name | capture(\"\\\\.(?<ext>deb|zip|tar\\\\.gz|tgz|xz)$\"; \"i\")) |
+    select(isempty(.ext) | not)
+] | first | \"\(.url) \(.ext)\"")
 
-    echo "Downloading .deb file from: $DEB_URL"
-    curl -sfLo "$DEB_FILE" "$DEB_URL"
+read -r DOWNLOAD_URL EXT <<<"$DOWNLOAD_URL"
 
-    echo "Installing the package..."
-    dpkg -i "$DEB_FILE"
-
-    if [ $? -eq 0 ]; then
-        echo "Package installed successfully."
-        rm "$DEB_FILE"
-    else
-        echo "Package installation failed. The .deb file is still in the current directory."
-        exit 1
-    fi
-}
-
-# Function to download and install from .zip
-install_zip() {
-    local ZIP_URL="$1"
-    local ZIP_FILE="${REPO}_latest_${ARCH}.zip"
-
-    echo "Downloading .zip file from: $ZIP_URL"
-    curl -sfLo "$ZIP_FILE" "$ZIP_URL"
-
-    echo "Extracting the .zip file..."
-    unzip -o "$ZIP_FILE" -d "$INSTALL_DIR"
-
-    if [ $? -eq 0 ]; then
-        echo "Binaries extracted successfully to $INSTALL_DIR"
-        rm "$ZIP_FILE"
-    else
-        echo "Extraction failed. The .zip file is still in the current directory."
-        exit 1
-    fi
-}
-
-# Function to download and install from .tar.gz
-install_tgz() {
-    local TGZ_URL="$1"
-    local TGZ_FILE="${REPO}_latest_${ARCH}.tar.gz"
-
-    echo "Downloading .tar.gz file from: $TGZ_URL"
-    curl -sfLo "$TGZ_FILE" "$TGZ_URL"
-
-    echo "Extracting the .tar.gz file..."
-    install -d "$INSTALL_DIR"
-    tar xf "$TGZ_FILE" -C "$INSTALL_DIR"
-
-    if [ $? -eq 0 ]; then
-        echo "Binaries extracted successfully to $INSTALL_DIR"
-        rm "$TGZ_FILE"
-    else
-        echo "Extraction failed. The .tar.gz file is still in the current directory."
-        exit 1
-    fi
-}
-
-# Try to find a .deb file first
-DEB_URL=$(echo "$LATEST_RELEASE" | jq -r ".assets[] | select(.name | test(\"(${ARCH_VARIANTS})\\\\.deb$\"; \"i\")) | .browser_download_url")
-if [ -n "$DEB_URL" ]; then
-    install_deb "$DEB_URL"
-    exit
+if [[ -z "$DOWNLOAD_URL" || -z "$EXT" ]]; then
+    echo "No suitable file found for architecture: $ARCH (variants: $ARCH_VARIANTS)"
+    exit 1
 fi
 
-# If no .deb file, look for a .zip file
-ZIP_URL=$(echo "$LATEST_RELEASE" | jq -r ".assets[] | select(.name | test(\"-linux-(${ARCH_VARIANTS})\\\\.zip$\"; \"i\")) | .browser_download_url")
-if [ -n "$ZIP_URL" ]; then
-    install_zip "$ZIP_URL"
-    exit
-fi
+# Download asset
+TMPDIR=$(mktemp -d)
+cd "$TMPDIR"
 
-# If no .deb or .zip file, look for a .tar.gz or .tgz file
-TGZ_URL=$(echo "$LATEST_RELEASE" | jq -r ".assets[] | select(.name | test(\"-linux-(${ARCH_VARIANTS})\\\\.(tar\\\\.gz|tgz)$\"; \"i\")) | .browser_download_url")
-if [ -n "$TGZ_URL" ]; then
-    install_tgz "$TGZ_URL"
-    exit
-fi
+ASSET="asset.$EXT"
+curl -sfLo "$ASSET" "$DOWNLOAD_URL"
 
-echo "No suitable .deb or .zip or .tar.gz file found for architecture: $ARCH (variants: $ARCH_VARIANTS)"
-exit 1
+# Install asset
+case $EXT in
+deb)
+    dpkg -i "$ASSET"
+    ;;
+zip)
+    unzip -o "$ASSET"
+    find . -type f -executable -exec install {} "$INSTALL_DIR" \;
+    ;;
+tar.gz | tgz | xz)
+    tar xf "$ASSET"
+    find . -type f -executable -exec install {} "$INSTALL_DIR" \;
+    ;;
+esac
+
+echo "Successfully installed binaries"
+cd /
+rm -rf "$TMPDIR"
